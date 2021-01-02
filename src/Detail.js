49c751ee21c0bc0fd0f12c3a46aeb65e536a4494
developerpark2348 <developerpark2348@gmail.com>
2021-01-02 12:41:07 +0900

    UseEffect refactoring & 사라지는 창 만드는법.
    사라지는 창 만드는법
    1. UI가 보이고 안보이는걸 state로 저장한다. ex) let [ 창, 창변경 ] = useState(true);
    
    2. state가 true일때만 창을 출력하도록 삼항연산자를 사용한다.
    {창 === true
    ? 보일 html
    : null
    }
    
    3. let 변수 = setTimeout(()=>{ 창변경(false) }, 2000ms(2초) ); 로 식을 만든다.
    
    업데이트시에도 UseEffect가 재렌더링(업데이트)되기에 성능낭비가 생김.
    useEffect(()=>{
     let 변수 = setTimeout(()=> { 창변경(false) }, 2000(ms 2초) ); },
    대괄호를 useEffect식 마지막에 넣어줌.=>[]);
    
    *대괄호안에 state가 변경될때만 재렌더링(update)로 치고 실행하라. 라는 조건문이 만들어짐.
    * [] => 공백으로 조건을 만들시엔 컴포넌트 로드시 한번만 실행하는 코드를 만들어줌.
    
    버그 수정
    2000ms보다 빠르게 페이지를 벗어날경우 오류가 생길수있기에
    타이머 함수를 없에는 함수를 만들어줌.
    
    useEffect안
    retrun() => { clearTimeout(변수) }
    
    *return() : 페이지를 벗어날때 실행하는 useEffect
    
    *clearTimeout(타이머변수이름) :  타이머를 바로 해제할수있음.

--- src/Detail.js
+++ src/Detail.js
@@ -1,4 +1,6 @@
-import React, { useEffect } from 'react';
+/* eslint-disable */
+
+import React, { useState, useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import './Detail.css'
@@ -14,29 +16,33 @@ let 제목 = styled.h4`
 
 function Detail(props){
 
-    useEffect(()=>{
-        setTimeout(()=>{
-
-        }, 2000)
-
-        return function 함수명(){실행할코드}
-    });
-
+    let [alert, alert변경] = useState(true);
     let { id } = useParams();
+    let history = useHistory();
     let 찾은상품 = props.shoes.find(function(상품){
         return 상품.id == id
     })
-    let history = 
-    useHistory();
+
+    useEffect(()=>{
+        let 타이머 = setTimeout(()=>{
+          alert변경(false)
+          return ()=> { clearTimeout(타이머) }
+        }, 2000)
+    },[/*실행 조건을 넣는 칸. [alert, Data]=> alert,Data0가 변경시에만 실행. 여러개 가능 [](꽁백)시 로드되었을때 한번만 실행함.*/]);
 
     return(
       <div className="container">
           <박스>
             <제목 색상="black">상세페이지</제목>
           </박스>
-     <div className="my-alert2">
-         <p>재고가 얼마 남지 않았습니다.</p>
-     </div>
+
+      {
+        alert === true
+        ? (<div className="my-alert2">
+          <p>재고가 얼마 남지 않았습니다.</p>
+          </div>)
+        : null
+      }
             <div className="row">
               <div className="col-md-6">
                 <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
