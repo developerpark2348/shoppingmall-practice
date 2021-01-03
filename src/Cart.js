2b84845d41ee5039e92117bd2e331842983a0fa1
developerpark2348 <developerpark2348@gmail.com>
2021-01-03 15:36:10 +0900

    redux state 데이터 수정방법
    redux는 데이터 수정시 reducer를 따로 만들어준다.
    
    1. let 스테이트명 = [{id : 0, name : '멋진신발', quan : 2 }];
    
    2. function reducer(state = 스테이트명, 변수){
    if (변수.type === '수량증가') {
    let copy = [...state]
    copy[0].quan++;
    return copy
    } else {
    return state
    }
    }
    => <button onClick={()=>{ props.dispatch({type: '수량증가'}) }}> + </button>
    : 버튼 클릭시 type이 '수량증가'이기에 기존 스테이트에서 1을 더한값을 quan에 넣어출력한다.

--- src/Cart.js
+++ src/Cart.js
@@ -12,14 +12,20 @@ function Cart(props){
                     <th>#</th>
                     <th>상품명</th>
                     <th>수량</th>
-                    <th>변경</th>
+                    <th>증가</th>
+                    <th>감소</th>
                 </tr>
-                <tr>
-                    <td>1</td>
-                    <td>{ props.state[0].name }</td>
-                    <td>T</td>
-                    <td>Table cell</td>
-                </tr>
+                { props.state.map((a,i)=>{
+                    return (
+                    <tr key={i}>
+                        <td>{a.id}</td>
+                        <td>{a.name}</td>
+                        <td>{a.quan}</td>
+                        <td><button onClick={()=>{ props.dispatch({type: '수량증가'}) }}> + </button></td>
+                        <td><button onClick={()=>{ props.dispatch({type: '수량감소'}) }}> - </button></td>
+                    </tr>
+                    )
+                })}
             </Table>
 
             </div>
